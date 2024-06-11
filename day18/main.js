999a3b90f5316174861bf05b22e32fab7d533c0b
laibasiddique75 <laibasiddique111@gmail.com>
2024-06-11 16:46:33 +0500

    added

---
+++ day18/main.js
@@ -0,0 +1,26 @@
+// Q no 52
+var iPhone = {
+    make: "iphone",
+    model: "15 pro PTA approved",
+    specification: { storage: "128gb",
+        screenSize: "4.5 inches",
+        batteryLife: "8 hours"
+    }
+};
+console.log(iPhone);
+// Q no 53
+var developersSkills = {
+    languages: ["HTML", "CSS", "JAVASCRIPT"],
+    frameworks: ["React", "Angular", "Vue"],
+    tools: ["Git", "Webpack", "Docker"]
+};
+var languages = developersSkills.languages, frameworks = developersSkills.frameworks, tools = developersSkills.tools;
+console.log("Language:".concat(languages[1], ",Framework: ").concat(frameworks[0], ",Tool:").concat(tools[2]));
+// Q no 54
+function createObjectKey(key, value) {
+    var dynamicObject = {};
+    dynamicObject[key] = value;
+    return dynamicObject;
+}
+var usersCoices = createObjectKey("theme", "dark");
+console.log(usersCoices);
---
+++ day18/main.ts
@@ -0,0 +1,40 @@
+// Q no 52
+let iPhone = {
+    make:"iphone",
+    model:"15 pro PTA approved",
+ specification:{storage:"128gb",
+screenSize:"4.5 inches",
+batteryLife:"8 hours"
+ }
+};
+console.log(iPhone);
+
+// Q no 53
+
+let developersSkills = {
+    languages:["HTML","CSS","JAVASCRIPT"],
+    frameworks:["React","Angular","Vue"],
+    tools:["Git","Webpack","Docker"]
+
+};
+
+let {
+    languages,frameworks,tools
+} = developersSkills
+
+console.log(`Language:${languages[1]},Framework: ${frameworks[0]},Tool:${tools[2]}`);
+
+// Q no 54
+
+ function createObjectKey(key:string,value:string){
+    let dynamicObject = {};
+     dynamicObject[key] = value;
+    return dynamicObject;
+}
+let usersCoices =  createObjectKey("theme","dark");
+console.log(usersCoices);
+
+
+
+
+
