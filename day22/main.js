57769b6ab0d08681769c58627b2b54dbacb5f4ea
laibasiddique75 <laibasiddique111@gmail.com>
2024-06-13 02:32:27 +0500

    done

---
+++ day22/main.js
@@ -0,0 +1,15 @@
+// Q no 64
+function combineStringNumber(text, number) {
+    return text + number;
+}
+console.log(combineStringNumber("Age:", 20));
+// Q no 65
+function remainder(num1, num2) {
+    return num1 % num2;
+}
+console.log(remainder(7, 5));
+// Q no 66
+function valueCheck(value1, value2) {
+    return value1 && value2;
+}
+console.log(valueCheck(true, false));
---
+++ day22/main.ts
@@ -0,0 +1,34 @@
+// Q no 64
+
+ function combineStringNumber(text:string,number:number){
+ return text+number;
+ }
+
+ console.log(combineStringNumber("Age:",20));
+
+
+
+// Q no 65
+
+function remainder(num1:number,num2:number){
+return num1%num2 
+}
+
+console.log(remainder(7,5));
+
+
+// Q no 66
+
+function valueCheck(value1:boolean,value2:boolean){
+return value1 && value2
+}
+
+console.log(valueCheck(true,false));
+
+
+
+
+
+
+
+
