0c8838cb632db7619e067bba86c152e61c05475f
laibasiddique75 <laibasiddique111@gmail.com>
2024-06-12 23:35:56 +0500

    added

---
+++ day20/main.js
@@ -0,0 +1,29 @@
+// Q no 58
+function averageScore() {
+    var scores = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        scores[_i] = arguments[_i];
+    }
+    var total = scores.reduce(function (sum, score) { return sum + score; }, 0);
+    return total / scores.length;
+}
+console.log(averageScore(10, 20, 30, 40));
+// Q no 59
+function adder(valueToAdd) {
+    return function (number) {
+        return number + valueToAdd;
+    };
+}
+var addSix = adder(6);
+console.log(addSix(12));
+// Q no 60
+var userProfile = (function () {
+    var name = "laiba";
+    var age = 20;
+    return {
+        displayInfo: function () {
+            console.log("Name:".concat(name, ", Age:").concat(age));
+        }
+    };
+})();
+userProfile.displayInfo();
---
+++ day20/main.ts
@@ -0,0 +1,41 @@
+// Q no 58
+
+function averageScore (...scores:number[]):number{
+    let total = scores.reduce((sum , score) => sum + score , 0);
+    return total/scores.length;
+}
+
+console.log(averageScore(10,20,30,40));
+
+
+// Q no 59
+
+function adder (valueToAdd:number):(number) => number {
+    return function (number:number):number{
+        return number + valueToAdd;
+    };
+}  let addSix = adder(6);
+console.log(addSix(12));
+
+
+
+// Q no 60
+
+let userProfile = (function(){
+
+let name = "laiba";
+let age = 20;
+
+return {
+
+    displayInfo : function(){
+        console.log(`Name:${name}, Age:${age}`);
+        
+    }
+};
+})();
+
+userProfile.displayInfo();
+
+
+
