2a99013cabc29b90660feced05a04f0ea43904e7
laibasiddique75 <laibasiddique111@gmail.com>
2024-06-12 23:43:13 +0500

    added

---
+++ day21/main.js
@@ -0,0 +1,24 @@
+// Q no 61
+var VehicleType;
+(function (VehicleType) {
+    VehicleType[VehicleType["Car"] = 0] = "Car";
+    VehicleType[VehicleType["Motorcycle"] = 1] = "Motorcycle";
+    VehicleType[VehicleType["Rikshaw"] = 2] = "Rikshaw";
+})(VehicleType || (VehicleType = {}));
+console.log(VehicleType.Rikshaw);
+var student = {
+    name: "laiba",
+    age: 20,
+    courses: ["math", "english", "urdu"]
+};
+var circle = {
+    kind: "circle",
+    radius: 5
+};
+var reactangle = {
+    kind: "reactangle",
+    width: 20,
+    height: 30
+};
+console.log(circle);
+console.log(reactangle);
---
+++ day21/main.ts
@@ -0,0 +1,58 @@
+// Q no 61
+
+enum VehicleType {
+    Car,
+    Motorcycle,
+    Rikshaw
+}
+
+console.log(VehicleType.Rikshaw);
+
+
+// Q no 62
+interface Student {
+    name:string;
+age:number;
+courses:string[];
+
+
+}
+
+let student : Student = {
+ name:"laiba",
+ age:20,
+courses :["math","english","urdu"]
+};
+
+// Q no 63
+
+type Shape = {
+    kind:"circle"| "reactangle";
+    radius?:number;
+    width?:number;
+    height?:number;
+}
+
+let circle:Shape = {
+    kind:"circle",
+    radius:5
+
+}
+
+
+let reactangle:Shape= {
+    kind:"reactangle",
+    width:20,
+    height:30
+};
+
+
+console.log(circle);
+console.log(reactangle);
+
+
+
+
+
+
+
