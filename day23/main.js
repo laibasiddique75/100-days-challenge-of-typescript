d42ad55b0ea8d79b34ee27c395ae61ae2e4672e4
laibasiddique75 <laibasiddique111@gmail.com>
2024-06-14 16:23:20 +0500

    added

---
+++ day23/main.js
@@ -0,0 +1,28 @@
+// Q no 73
+// let number = 7;
+// console.log(`initial value: ${number}`);
+// number = 5;
+// console.log(`updated value: ${number}`);
+// Q no 74
+// let a = 5;
+// let b = 10;
+// console.log(`Before swap: a = ${a}, b = ${b}`);
+// // Swap the values
+// let temporary = a;
+// a = b;
+// b = temporary;
+// console.log(`After swap: a = ${a}, b = ${b}`);
+// Q no 75
+function compoundOperators() {
+    var x = 4;
+    console.log("Initial x : ".concat(x));
+    x += 2;
+    console.log("After addition: ".concat(x));
+    x -= 4;
+    console.log("After substraction : ".concat(x));
+    x *= 5;
+    console.log("After multiplication: ".concat(x));
+    x /= 7;
+    console.log("After division: ".concat(x));
+}
+compoundOperators();
---
+++ day23/main.ts
@@ -0,0 +1,51 @@
+// Q no 73
+
+// let number = 7;
+
+// console.log(`initial value: ${number}`);
+
+// number = 5;
+
+// console.log(`updated value: ${number}`);
+
+
+// Q no 74
+
+// let a = 5;
+// let b = 10;
+
+// console.log(`Before swap: a = ${a}, b = ${b}`);
+
+// // Swap the values
+// let temporary = a;
+// a = b;
+// b = temporary;
+
+// console.log(`After swap: a = ${a}, b = ${b}`);
+
+
+// Q no 75
+
+function compoundOperators(){
+    let x = 4;
+
+    console.log(`Initial x : ${x}`);
+
+    x += 2;
+
+    console.log(`After addition: ${x}`);
+
+    x -= 4;
+
+    console.log(`After substraction : ${x}`);
+
+    x *= 5;
+    
+    console.log(`After multiplication: ${x}`);
+
+    x /= 7;
+    console.log(`After division: ${x}`);
+
+    
+}
+compoundOperators();
