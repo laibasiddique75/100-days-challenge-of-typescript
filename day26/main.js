18c4dc6a9633ef7ed7fab9c6393d72930b40a2e3
laibasiddique75 <laibasiddique111@gmail.com>
2024-06-17 02:09:29 +0500

    added

---
+++ day26/main.js
@@ -0,0 +1,17 @@
+// Q no 82
+// function stringLength(str: string):number{
+// return str.length;
+// }
+// console.log(stringLength("laiba"));
+// Q no 83
+// function convertedCase(str:string){
+// let upperStr= str.toUpperCase();
+// let lowerStr= str.toLowerCase();
+// console.log("Uppercase:",upperStr,"Lowercase:",lowerStr);
+// }
+// convertedCase("Hello Zeeshan");
+// Q no 84
+function replaceJavascriptWithTypescript(sentence) {
+    return sentence.replace(/javascript/g, "Typescript");
+}
+console.log(replaceJavascriptWithTypescript("I Love javascript and it is awesome"));
