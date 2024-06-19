ff46530afb39dd8cd4f40eed1a38c690687dfb1e
laibasiddique75 <laibasiddique111@gmail.com>
2024-06-19 20:52:53 +0500

    added

---
+++ day28/main.js
@@ -0,0 +1,17 @@
+"use strict";
+// Q no 85
+Object.defineProperty(exports, "__esModule", { value: true });
+// function findCodePosition(str:string):number{
+// return str.indexOf("code");
+// }
+//  console.log(findCodePosition("learn to code with typescript"));
+// Q no 86
+// function wordChecking(str:string):boolean{
+// return str.includes("Zeeshan");
+// }
+// console.log(wordChecking("I  love coding and chatting with Zeeshan!"));
+// Q no 87
+function firstTenCharacters(str) {
+    return str.substring(0, 10);
+}
+console.log(firstTenCharacters("Hello coders~"));
---
+++ day28/main.ts
@@ -0,0 +1,29 @@
+// Q no 85
+
+// function findCodePosition(str:string):number{
+// return str.indexOf("code");
+// }
+
+//  console.log(findCodePosition("learn to code with typescript"));
+
+// Q no 86
+// function wordChecking(str:string):boolean{
+// return str.includes("Zeeshan");
+// }
+
+// console.log(wordChecking("I  love coding and chatting with Zeeshan!"));
+
+// Q no 87
+
+// function firstTenCharacters(str:string):string{
+//     return str.substring(0,10);
+// }
+
+// console.log(firstTenCharacters("Hello coders"));
+
+
+
+
+
+
+
