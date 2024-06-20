a0fc2abc090284f477bc3c10a600631b1705661a
laibasiddique75 <laibasiddique111@gmail.com>
2024-06-21 01:25:59 +0500

    added

---
+++ day29/main.js
@@ -0,0 +1,19 @@
+// Q no 91
+// let favouriteFruits : string [] = ["Apple","Mango","Orange"];
+// favouriteFruits.push("Cherry");
+// console.log(favouriteFruits);
+// Q no 92
+// function removeElement<T>(arr:T[]):T | undefined{
+// return arr.pop();
+// }
+// let fruits:string[] = ["Apple","banana","mango"];
+// console.log(removeElement(fruits));
+// Q no 93
+function fruit(fruits) {
+    var index = fruits.indexOf("Banana");
+    if (index !== -1)
+        fruits[index] = "Mango";
+}
+var frt = ["Apple", "banana", "Orange"];
+console.log(frt);
+console.log(frt);
