778edbd79e4736a2656ca16be5b606a0076677c8
laibasiddique75 <laibasiddique111@gmail.com>
2024-06-23 01:02:17 +0500

    added

---
+++ day31/main.ts
@@ -0,0 +1,35 @@
+// Q no 97
+
+// function currentDateFormat(){
+// let date = new Date()
+// let day = date.getDate()
+// let month = date.getMonth() + 1
+// let year = date.getFullYear()
+// return `day:${day}/ month: ${month}/ year:${year}`
+
+// }
+
+// console.log(currentDateFormat());
+
+
+// //Q no 98
+
+// function newYear(){
+//     let currentDate = new Date()
+//     let newYearDate = new Date(2025,0,1)
+//     let difference = newYearDate.getTime()-currentDate.getTime()
+//     let days = Math.floor(difference / (1000*60*60*24))
+//     return days
+// }
+
+// console.log(newYear()+ "remaing days");
+
+// Q no 99
+
+function birthDay(month:number,day:number){
+    let date = new Date()
+    let birthday =  new Date(date.getFullYear(),month + 1,day)
+    return birthday
+}
+ console.log(birthDay(6,18));
+ 
