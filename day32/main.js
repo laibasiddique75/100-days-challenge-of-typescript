09df7a54f1e734dffa9ef8b20b8b23de2ecd6efc
laibasiddique75 <laibasiddique111@gmail.com>
2024-06-24 15:35:09 +0500

    added

---
+++ day32/main.js
@@ -0,0 +1,11 @@
+// // Q no 100
+// let squareRoot: number = Math.sqrt(1728) ;
+// console.log(squareRoot);
+// Q no 101
+function generateInteger() {
+    return Math.floor(Math.random() * 10);
+}
+console.log(generateInteger());
+// Q no 102
+var num = Math.abs(-10 - 10);
+console.log(num);
---
+++ day32/main.ts
@@ -0,0 +1,31 @@
+
+
+
+// // Q no 100
+// let squareRoot: number = Math.sqrt(1728) ;
+
+// console.log(squareRoot);
+
+// Q no 101
+
+function generateInteger() : number{
+    return Math.floor(Math.random() * 10) 
+
+}
+
+
+console.log(generateInteger());
+
+
+
+
+// Q no 102
+
+let  num : number = Math.abs(-10 - 10);
+
+console.log(num);
+
+
+
+
+
