4c8c4732545218047eb2ac6362d00b5fbcf32783
laibasiddique75 <laibasiddique111@gmail.com>
2024-06-25 19:12:10 +0500

    added

---
+++ day33/main.js
@@ -0,0 +1,20 @@
+// Q no 103
+// function booleanValue():boolean{
+// return Math.random() > 0.5;
+// }
+// console.log(booleanValue());
+// Q no 104
+// function getRandomHexaColor(): string {
+//     const letters = 'ffffff';
+//     let color = '#';
+//     for (let i = 0; i < 6; i++) {
+//         color += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return color;
+// }
+// console.log(getRandomHexaColor()); 
+// Q no 105
+function diceRoll() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+console.log(diceRoll());
