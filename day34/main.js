840b935ae4e34022e58ef469eee199187e46ee8c
laibasiddique75 <laibasiddique111@gmail.com>
2024-06-26 21:35:01 +0500

    added

---
+++ day34/main.js
@@ -0,0 +1,20 @@
+// Q no 106
+// function isLeapYear(year: number) {
+//     let years = year % 4 === 0 && year % 100 !== 0
+//     return years
+// }
+// console.log(isLeapYear(2020));
+// console.log(isLeapYear(2021)); 
+// console.log(isLeapYear(1900)); 
+// Q no 107
+//     function isDivisibleBy2And3(num: number): boolean {
+//         return num % 2 === 0 && num % 3 === 0;
+//     }
+//     console.log(isDivisibleBy2And3(12));
+// console.log(isDivisibleBy2And3(9));
+// Q no 108
+function stringCaseSensitive(str1, str2) {
+    return str1.toLowerCase() === str2.toLowerCase();
+}
+console.log(stringCaseSensitive("HELLO", "hello"));
+console.log(stringCaseSensitive("ZEESHAN", "zeeshan"));
