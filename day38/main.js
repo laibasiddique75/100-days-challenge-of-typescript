4f3e4c94ec4b9e607222f482d44e4fb40bcdb275
laibasiddique75 <laibasiddique111@gmail.com>
2024-07-01 18:07:34 +0500

    added

---
+++ day38/main.js
@@ -0,0 +1,15 @@
+// 118
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+// 119
+// let count = 0;
+// while (count < 5){
+//     console.log("HELLO JANAB");
+//      count ++;
+// }
+// 120
+var arr = ["zeeshan", "heavy bike", "sports bike"];
+for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i][1]);
+}
