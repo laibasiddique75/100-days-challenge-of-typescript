cc01256a808b0072e9922c867331b8647ef29fc5
laibasiddique75 <laibasiddique111@gmail.com>
2024-07-02 20:40:36 +0500

    added

---
+++ day39/main.ts
@@ -0,0 +1,34 @@
+// //Q no 121
+// for (let i = 1; i <= 10; i++) {
+//     if (i === 5) {
+//         continue;
+//     }
+//     console.log(i); 
+// }
+
+// // Q no 122
+
+// let i = 10;
+
+// while (i > 0) {
+//     if (i === 5) {
+//         break; 
+//     }
+//     console.log(i); 
+//     i--; 
+// }
+
+
+// Q no 123
+
+let str = "zeeshan";
+let vowels = "aeiouAEIOU";
+
+for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+        console.log(`First vowel found: ${str[i]}`);
+        break;
+    }
+}
+
+
