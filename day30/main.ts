37e056a02aa9a1b3d68fa74ce717e5e9409757f8
laibasiddique75 <laibasiddique111@gmail.com>
2024-07-23 17:57:24 +0500

    added

--- day30/main.ts
+++ day30/main.ts
@@ -20,6 +20,20 @@
 
 // Q no 96
 
+// function reduceArray(numbers :number[]):number{
+//     return numbers.reduce((accumulator,current) => accumulator + current,0);
+// }
+
+
+
+// const numbers : number[] = [1,2,3,4];
+
+// console.log(reduceArray(numbers));
+
+
+
+
+
 function reduceArray(numbers :number[]):number{
     return numbers.reduce((accumulator,current) => accumulator + current,0);
 }
