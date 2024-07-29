2a6c1e5f8614e1fc97b504b1817b87506c7f2b3e
laibasiddique75 <laibasiddique111@gmail.com>
2024-07-29 18:22:25 +0500

    added

--- day36/main.ts
+++ day36/main.ts
@@ -1,4 +1,16 @@
 // Q no 112
+// let countryCapitals: Map<string, string> = new Map();
+
+
+// countryCapitals.set("France", "Paris");
+// countryCapitals.set("Japan", "Tokyo");
+// countryCapitals.set("Brazil", "Brasilia");
+
+
+// console.log(countryCapitals);
+
+
+
 let countryCapitals: Map<string, string> = new Map();
 
 
