5424b242e0c638f50a969bb6db817ff458037573
laibasiddique75 <laibasiddique111@gmail.com>
2024-07-31 23:33:55 +0500

    added

--- day38/main.ts
+++ day38/main.ts
@@ -17,14 +17,18 @@
 // }
 
 // 120
+// var arr = ["zeeshan","heavy bike","sports bike"];
+// for(var i=0;i<arr.length;i++){
+// console.log(arr[i]);
+
+// }
+
+
 var arr = ["zeeshan","heavy bike","sports bike"];
 for(var i=0;i<arr.length;i++){
 console.log(arr[i]);
 
 }
-
-
-
 
 
 
